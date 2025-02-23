c497a4b022dd3442248ecabb7741ef3f15b29f57
Shahaz <ali.ghaderipour@gmail.com>
2025-02-23 12:43:02 +0330

    dunder2

--- 6-special_methods2(dunder methods).py
+++ 6-special_methods2(dunder methods).py
@@ -1,5 +1,5 @@
-print(3+4)
-print(int.__add__(3,4))
+print(3 + 4)
+print(int.__add__(3, 4))
 
 print('a' + 'b')
 print(str.__add__('a', 'b'))
@@ -8,6 +8,13 @@ print(str.__add__('a', 'b'))
 class Employee:
     def __init__(self, name, pay):
         self.name = name
-        self.pay pay
+        self.pay = pay
+
+    def __add__(self, other):
+        return "your total payment is {}".format(self.pay + other.pay)
+
 
 emp_1 = Employee('John', 100)
+emp_2 = Employee('Sue', 200)
+
+print(emp_1 + emp_2)
