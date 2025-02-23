d6add04f294a60756065dc238e8e26b892ce35a4
shahaz <ali.ghaderipour@gmail.com>
2025-02-23 20:43:50 +0330

    till decorators

--- 6-special_methods2(dunder methods).py
+++ 6-special_methods2(dunder methods).py
@@ -4,6 +4,8 @@ print(int.__add__(3, 4))
 print('a' + 'b')
 print(str.__add__('a', 'b'))
 
+print(len('test'))
+print('test'.__len__())
 
 class Employee:
     def __init__(self, name, pay):
@@ -13,8 +15,15 @@ class Employee:
     def __add__(self, other):
         return "your total payment is {}".format(self.pay + other.pay)
 
+    # if you comment __add__ dunder method, you get error since you cannot add two instances
+
+    def __len__(self):
+        return len(self.name)+1000
 
 emp_1 = Employee('John', 100)
 emp_2 = Employee('Sue', 200)
 
 print(emp_1 + emp_2)
+
+print(len(emp_1))
+
---
+++ 7-property decorators.py
@@ -0,0 +1,25 @@
+class Employee:
+    def __init__(self, first, last):
+        self.first = first
+        self.last = last
+
+    @property
+    def email(self):
+       return self.first + '.' + self.last + '@gmail.com'
+
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+emp_1 = Employee('John', 'Smith')
+print(emp_1.first)
+print(emp_1.email)
+print(emp_1.fullname())
+
+emp_1.first = 'Jimmy'
+print('chanting first name to jimmy')
+print(emp_1.first)
+print(emp_1.email)
+print(emp_1.fullname())
+
+# we changed the name but it did not get updated for "email"
+
